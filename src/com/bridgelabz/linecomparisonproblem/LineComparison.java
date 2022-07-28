db18aee1df1fbda4b80e443fb6450280c3824d2e
iamtalib13 <talibsheikh16@gmail.com>
2022-07-29 02:52:42 +0530

    Added Welcome Message

---
+++ src/com/bridgelabz/linecomparisonproblem/LineComparison.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linecomparisonproblem;
+
+public class LineComparison {
+    public static void main(String[] args) {
+
+
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
+
