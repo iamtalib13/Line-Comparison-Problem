bfedd222bc4c8a7aa83e269c2a26cf6675cf945c
iamtalib13 <talibsheikh16@gmail.com>
2022-07-29 12:10:32 +0530

    Added - Check Equility of Two Lines

--- src/com/bridgelabz/linecomparisonproblem/LineComparison.java
+++ src/com/bridgelabz/linecomparisonproblem/LineComparison.java
@@ -29,10 +29,25 @@ public class LineComparison {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
+        LineComparison LC1 = new LineComparison();
+        LineComparison LC2 = new LineComparison();
 
-        LineComparison LC = new LineComparison();
-        LC.getLinePoint();
-        System.out.println("Length of Lines : "+LC.getLengthOfLine());
+        LC1.getLinePoint();
+        LC2.getLinePoint();
+
+        double line1Length = LC1.getLengthOfLine();
+        double line2Length = LC2.getLengthOfLine();
+
+        System.out.println("Length of Line1 : "+line1Length);
+        System.out.println("Length of Line2 : "+line2Length);
+
+        if(line1Length == line2Length) {
+            System.out.println("Length of Line1 and Line2 are Equal");
+        } else {
+            System.out.println("Length of Line1 and Line2 are Not Equal");
+        }
+
+
     }
 }
 
